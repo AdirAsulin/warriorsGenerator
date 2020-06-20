7faca84fa680ca4afb06c800be9aeae246631b08
AdirAsulin <AdirAsulin1998@gmail.com>
2020-06-20 18:36:44 +0300

    all_the_task_works_without_the_bonus

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const types = ["archer","axeman","swordman"]
-const level = []
-const power
-const damage
-const image
+const types = ["archer", "axeman", "swordman"];
+const warriorsImage = {
+    archer: "https://chupacdn.s3.amazonaws.com/catalog/product/cache/1/thumbnail/1280x/17f82f742ffe127f42dca9de82fb58b1/a/r/archer-game-character-sprites-9162_imgs_9162_1.jpg",
+    swordman: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQo-eP1hGGtvAmBaGTdDE96SZYNstdkXW_TaJxNUaR1-zGw6Xh6&usqp=CAU",
+    axeman: "https://cdnb.artstation.com/p/assets/images/images/002/968/783/large/nikolay-naydenov-axeman1.jpg?1467890079",
+};
+
+function warriorGenerator() {
+    const level = Math.floor(Math.random() * 10);
+    const power = Math.floor(Math.random() * 100);
+    const type = types[Math.floor(Math.random() * types.length)]
+    const damage = level * power;
+    const image = warriorsImage
+    return {
+        level,
+        power,
+        damage,
+        image,
+        type
+    }
+}
+
+function drawWarrior(numberOfwarriors) {
+    const warriorsArray = []
+    for (let index = 0; index < numberOfwarriors; index++) {
+        warriorsArray.push(warriorGenerator())
+    }
+    return warriorsArray
+}
+const createBtn = document.getElementById("createBtn");
+createBtn.addEventListener("click", draw)
+
+function draw() {
+    const numberOfwarriors = document.getElementById("warriorsInputNumber").value
+    const result = drawWarrior(numberOfwarriors)
+    console.log(result);
+}
